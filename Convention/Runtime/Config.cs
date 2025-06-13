cc986d2c1f9fad5ac68d4e6f26eebbdc22c9d278
NINEMINEsigma <1371605831@qq.com>
2025-06-13 10:52:21 +0800

    添加项目文件。

---
+++ Convention/Runtime/Config.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convention
+{
+    namespace SAL
+    {
+        public interface IHasCheckMethod
+        {
+            bool Check();
+        }
+
+        public class ReturnValue { }
+
+        public enum ComparedFlag
+        {
+            Greater, Less, Equal, GreaterOrEqual, LessOrEqual, NotEqual
+        }
+
+        [System.AttributeUsage(AttributeTargets.ReturnValue, Inherited = true, AllowMultiple = true)]
+        public sealed class SuccessAttribute : Attribute, IHasCheckMethod
+        {
+            private List<object> exprs = new();
+
+            public bool Check()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+    }
+
+    public static class PlatformIndictaor
+    {
+    }
+}
