f850030d10b7984117db9665095f53ce1828bf4f
ninemine <1371605831@qq.com>
2025-10-11 09:53:30 +0800

    Save

---
+++ Convention/[RScript]/Matcher/BreakMatcher.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Convention.RScript.Matcher
+{
+    public class BreakMatcher : IRSentenceMatcher
+    {
+        public bool Match(string expression, ref RScriptSentence sentence)
+        {
+            Regex LabelRegex = new(@"^break\(\s*(.+)\s*\)$");
+            var LabelMatch = LabelRegex.Match(expression);
+            if (LabelMatch.Success)
+            {
+                sentence.mode = RScriptSentence.Mode.Label;
+                sentence.content = LabelMatch.Groups[1].Value;
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Convention/[RScript]/Matcher/NamespaceMater.cs
@@ -0,0 +1,20 @@
+﻿namespace Convention.RScript.Matcher
+{
+    public class NamespaceMater : IRSentenceMatcher
+    {
+        public bool Match(string expression, ref RScriptSentence sentence)
+        {
+            if (expression == "{")
+            {
+                sentence.mode = RScriptSentence.Mode.EnterNamespace;
+                return true;
+            }
+            else if (expression == "}")
+            {
+                sentence.mode = RScriptSentence.Mode.ExitNamespace;
+                return true;
+            }
+            return false;
+        }
+    }
+}
