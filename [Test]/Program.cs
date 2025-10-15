9d7fbf9786381d79bdd6182c64b3cc50bf6f3d05
ninemine <1371605831@qq.com>
2025-10-16 00:39:45 +0800

    Update RScript

--- [Test]/Program.cs
+++ [Test]/Program.cs
@@ -15,23 +15,14 @@ public class Program
 double i;
 i = 2.0;
 label(test);
-{
-    goto(true,func1);
-    goto(100>i,test);
-}
-double result;
-result = i;
+goto(true,func1);
+goto(100>i,test);
 
-goto(true,end);
-
-label(func1);
+namespace(func1)
 {
     i = Pow(i,2.0);
 }
-back(true);
-
-label(end);
 ", import);
-        Console.WriteLine($"Script executed successfully. Result: {result["result"].data}");
+        Console.WriteLine($"Script executed successfully. Result: {result["i"].data}");
     }
 }
