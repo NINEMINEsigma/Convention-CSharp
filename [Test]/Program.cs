8d6f96b99ab2b08023703ef8fdcaee0be707c6ff
ninemine <1371605831@qq.com>
2025-10-16 11:29:50 +0800

    Save

--- [Test]/Program.cs
+++ [Test]/Program.cs
@@ -3,25 +3,35 @@ using System;
 
 public class Program
 {
+    static class Test
+    {
+        public static object Func(object x)
+        {
+            Console.WriteLine(x);
+            return x;
+        }
+    }
+
     static void Main(string[] args)
     {
         RScriptEngine engine = new();
         RScriptImportClass import = new()
         {
             typeof(Math),
-           
+            typeof(ExpressionMath),
+            typeof(Test)
         };
-        import.Add(typeof(ExpressionMath));
         var result = engine.Run(@"
 int i;
-i = 2.2;
+i = 2;
 label(test);
 goto(true,func1);
 goto(100>i,test);
 
 namespace(func1)
 {
-    i = Pow(i,2.0);
+    i = Pow(i,2);
+    Func(i);
 }
 ", import);
         Console.WriteLine($"Script executed successfully. Result: {result["i"].data}");
